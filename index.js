63f4bdc26d2e6433677028237a3332801d2de632
bleronajashanica <117457583+bleronajashanica@users.noreply.github.com>
2024-04-04 01:38:56 +0200

    installing necessary packages

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app=express();
 const dotenv=require("dotenv").config();
 const PORT=process.env.PORT||4000;
 
+app.use("/",(req,res)=>{
+    res.send("Hello from the server side!");
+});
+
+
 app.listen(PORT,() =>{
     console.log(`Server is running at PORT ${PORT}`);
-
 });
