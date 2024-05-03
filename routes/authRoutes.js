f19525daa75ef727724e0079b0fa9e75cc54c405
Blerona Jashanica <122100952+bleronaj@users.noreply.github.com>
2024-05-04 00:59:10 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { 
     registerController,
-    loginController
+    loginController,
+   testController,
 }
     from "../controllers/authController.js";
-
-
+    import { 
+        requireSignIn,
+        isAdmin,
+    } 
+        from "../middlewares/authMiddleware.js";
 //router object
 const router = express.Router();
 
@@ -16,5 +20,8 @@ router.post('/register', registerController);
 
 //LOGIN // METHOD POST
 router.post('/login',loginController);
+
+//test routes
+router.get('/test',requireSignIn,isAdmin,testController);
 
 export default router;
