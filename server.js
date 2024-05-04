6647decfd07ae37bf2197eb97a378c1c5739962f
Blerona Jashanica <122100952+bleronaj@users.noreply.github.com>
2024-05-04 23:56:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,34 +2,42 @@ import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js';
+import cors from 'cors';
+
 
 //configure env
 dotenv.config();
 
+//rest object
+const app=express();
+
 //database config
 connectDB();
 
-//rest object
-const app = express();
-
 //middlewares
+app.use(cors())
 app.use(express.json())
-app.use(morgan('dev'))
+app.use(morgan('dev')) 
 
 //routes
-app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/auth",authRoutes);
+//database config
+connectDB();
 
 //rest api
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome to ecommerce app</h1>");
+app.get("/",(req, res)=>{
+    res.send('<h1>Welcome to ecommerce app</h1>');
 });
 
 //PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 //run listen
 app.listen(PORT, () => {
     console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
+
+
+
