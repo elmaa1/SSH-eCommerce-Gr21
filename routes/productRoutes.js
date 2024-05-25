15ce4071dbfa42a56dc998131e9305c1a233607f
elmaa1 <167031496+elmaa1@users.noreply.github.com>
2024-05-25 21:37:00 +0200

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -73,5 +73,13 @@ router.get("/braintree/token", braintreeTokenController);
 
 //payments
 router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
+//payments routes
+//token
+router.get("/braintree/token", braintreeTokenController);
+
+//payments
+router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
+
+
 
 export default router;
