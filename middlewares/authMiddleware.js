4d83ea96d296f227a8db89c1dd522048024dbed1
elmaa1 <167031496+elmaa1@users.noreply.github.com>
2024-05-26 22:36:18 +0200

    Added error responses (#43)

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,39 +1,42 @@
-import JWT  from "jsonwebtoken";
-//protected routes token base
-export const requireSignIn= async(req,res,next)=>{
-    try{
-        const decode=JWT.verify(
-        req.headers.authorization,
-        process.env.JWT_SECRET
-    );
-    req.user=decode;
-next();
-}
-catch(error){
-    console.log(error);
-}
-};
+import JWT from "jsonwebtoken";
 
-//admin access
-export const isAdmin=async(req,res,next)=>{
-    try{
-        const user=await userModel.findById(req.user._id);
-        if(user.role !==1){
-            return res.status(401).send({
-                success:false,
-                message:"UnAuthorized Access",
-            });
-        }
-        else{
-            next();
-        }
-    }
-    catch(error){
+// Protected routes token base
+export const requireSignIn = async (req, res, next) => {
+    try {
+        const decode = JWT.verify(
+            req.headers.authorization,
+            process.env.JWT_SECRET
+        );
+        req.user = decode;
+        next();
+    } catch (error) {
         console.log(error);
         res.status(401).send({
-            success:false,
-            error,
-            message:"Error in admin middleware",
+            success: false,
+            message: "Invalid or expired token",
         });
     }
-}
+};
+
+// Admin access
+export const isAdmin = async (req, res, next) => {
+    try {
+        const user = await userModel.findById(req.user._id);
+        if (user.role !== 1) {
+            return res.status(401).send({
+                success: false,
+                message: "Unauthorized Access",
+            });
+        } else {
+            next();
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error in admin middleware",
+        });
+    }
+};
+
